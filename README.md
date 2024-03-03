ac6aa73641f3056ac03b7b47f47f32ce3b7b2ad5
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2024-03-03 23:27:21 +0900

    WIP

--- README.md
+++ README.md
@@ -2,14 +2,46 @@
 
 카카오톡 챗봇 스킬서버 for 공포 탐욕 지수([Fear & Greed Index](https://edition.cnn.com/markets/fear-and-greed))
 
+(WIP)
+
+- Cloudflare Workers
+- Cloudflare Durable Objects
+
 ## Develop
 
 ```sh
 pnpm run dev
 ```
+
 Start dev server (localhost:8787)
 
 ## Deploy
+
 ```sh
 pnpm run deploy
 ```
+
+## etc
+
+### Configure Secrets
+
+Bot proxy -> Skill server 인증용 헤더
+
+```sh
+pnpm wrangler secret put PRESHARED_AUTH_HEADER_KEY
+pnpm wrangler secret put PRESHARED_AUTH_HEADER_VALUE
+```
+
+(참고: https://developers.cloudflare.com/workers/configuration/secrets/)
+
+### Log
+
+```sh
+pnpm wrangler tail
+```
+
+> [!NOTE]
+> request object 로깅은 `.headers`, `.json()` 등으로 읽어서 해야 하고,
+> request body는 한 번만 읽을 수 있음. 로깅등 필요하면 clone해서 사용 필요한듯
+
+(참고: https://developers.cloudflare.com/workers/examples/logging-headers/)
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
 import { SimpleText, SkillResponse, Template } from 'kakao-chatbot-templates';
 
+// ref: https://developers.cloudflare.com/workers/examples/protect-against-timing-attacks/
+const encoder = new TextEncoder();
+const isEqual = (a: string, b: string): Boolean => {
+	const aBytes = encoder.encode(a);
+	const bBytes = encoder.encode(b);
+
+	if (aBytes.byteLength !== bBytes.byteLength) {
+		return false;
+	}
+
+	return crypto.subtle.timingSafeEqual(aBytes, bBytes);
+}
+
+
 export interface Env {
 	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
 	// MY_KV_NAMESPACE: KVNamespace;
@@ -15,40 +29,126 @@ export interface Env {
 	//
 	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
 	// MY_QUEUE: Queue;
+
+	USER_STORE: DurableObjectNamespace;
+
+	// auth
+	PRESHARED_AUTH_HEADER_KEY: string;
+	PRESHARED_AUTH_HEADER_VALUE: string;
 }
 
 // req/res 형식 참고: https://kakaobusiness.gitbook.io/main/tool/chatbot/main_notions/setting_parameter#payload
 
 export default {
+	async scheduled(event: ScheduledController, env: Env, ctx: ExecutionContext) {
+		console.log("cron processed");
+
+		// Event API 사용
+		// ref: https://kakaobusiness.gitbook.io/main/tool/chatbot/main_notions/event-api#send_eventapi_policy
+		// 흠..
+	},
+
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-
-		const FGI_JSON_PATH = "https://sh-cho.github.io/fear-and-greed-notifier/fgi_output.json";
-
-		async function gatherResponse(response) {
-			const { headers } = response;
-			const contentType = headers.get("content-type") || "";
-			if (contentType.includes("application/json")) {
-				return JSON.stringify(await response.json(), null, 2);
-			}
-			return response.text();
+		// 챗봇 요청은 무조건 POST로만 들어옴
+		if (request.method !== "POST") {
+			return new Response("Method Not Allowed", { status: 405, headers: { Allow: "POST" } });
 		}
 
-		const init = {
-			headers: {
-				"content-type": "application/json;charset=UTF-8",
-			},
-		};
+		// auth
+		const PRESHARED_AUTH_HEADER_KEY = env.PRESHARED_AUTH_HEADER_KEY;
+		const PRESHARED_AUTH_HEADER_VALUE = env.PRESHARED_AUTH_HEADER_VALUE;
+		const psk = request.headers.get(PRESHARED_AUTH_HEADER_KEY);
+		if (psk === null || isEqual(psk, PRESHARED_AUTH_HEADER_VALUE) === false) {
+			return new Response("Forbidden", { status: 403 });
+		}
 
-		const response = await fetch(FGI_JSON_PATH, init);
-		const fgi = await gatherResponse(response);
+		/***************************************************************************
+		 * handlers
+		 **************************************************************************/
+		async function getFgi() {
+			const FGI_JSON_PATH = "https://sh-cho.github.io/fear-and-greed-notifier/fgi_output.json";
 
-		const template = new Template([
-			new SimpleText(fgi)
-		]);
-		const skillResponseJSON = JSON.stringify(new SkillResponse(template).render());
+			async function gatherResponse(response) {
+				const { headers } = response;
+				const contentType = headers.get("content-type") || "";
+				if (contentType.includes("application/json")) {
+					return JSON.stringify(await response.json(), null, 2);
+				}
+				return response.text();
+			}
 
-		console.log(skillResponseJSON);
+			const init = {
+				headers: {
+					"content-type": "application/json;charset=UTF-8",
+				},
+			};
+			const response = await fetch(FGI_JSON_PATH, init);
+			const requestClone = request.clone();
+			const fgi = await gatherResponse(response);
 
-		return new Response(skillResponseJSON, init);
+			const template = new Template([
+				new SimpleText(fgi)
+			]);
+			const skillResponseJSON = JSON.stringify(new SkillResponse(template).render());
+			// console.log(skillResponseJSON);
+
+			console.log(JSON.stringify(await requestClone.json()));
+			return new Response(skillResponseJSON, init);
+		}
+
+		//
+		async function setAlarm() {
+			// const id = env.USER_STORE.idFromName("alarm");
+			// const stub = env.USER_STORE.get(id);
+
+			console.log(JSON.stringify(await request.json()));
+
+			return new Response("Not Implemented", { status: 501 });
+		}
+
+		/***************************************************************************
+		 * NOTE
+		 * - Action type: body.action.params.type
+		 * - chatId: body.userRequest.chatId
+		 **************************************************************************/
+		try {
+			// ref: https://kakaobusiness.gitbook.io/main/tool/chatbot/main_notions/setting_parameter
+			const requestClone = request.clone();
+			const requestJson: any = await requestClone.json();
+			const actionType: string = requestJson?.action?.params?.type || "";
+
+			switch (actionType) {
+				case "get":
+					return await getFgi();
+
+				case "set-alarm":
+					return setAlarm();
+
+				default:
+					return new Response("Bad Request", { status: 400 });
+			}
+		} catch (e) {
+			console.error("Request ID: ", (request.headers.get("X-Request-Id") || ""), ", err:", e);
+			return new Response("Bad Request", { status: 400 });
+		}
+
+
 	},
-};
+} satisfies ExportedHandler<Env>;
+
+
+// Durable Object
+
+export class UserStore {
+	state: DurableObjectState;
+
+	constructor(state: DurableObjectState, env: Env) {
+		this.state = state;
+	}
+
+	async fetch(request: Request) {
+		// TODO
+
+		return new Response("", { status: 200 });
+	}
+}
